679fc4718d627e2f5215628d31d697c3040f1f62
Christian <cabarcav25@gmail.com>
2021-01-28 10:42:13 -0600

    new commit

--- cypress/integration/todomvc.spec.js
+++ cypress/integration/todomvc.spec.js
@@ -1,5 +1,5 @@
 it('should navigate to the TodoMVC app', () => {
-cy.visit('http://todomvc-app-for-testing.surge.sh/no-et')
+cy.visit('http://todomvc-app-for-testing.surge.sh/')
 })
 // this ^ test receives 2 params:
 // the name of the test, and the function with test code
